74b686cc3be0c7c89732e4ac96254093da059348
Dayal hari <dayal.hari_bca24@gla.ac.in>
2025-08-29 09:12:46 +0530

    Update divide by 3 and last digit 4.py

--- divide by 3 and lst digit 4.py
+++ divide by 3 and lst digit 4.py
@@ -2,4 +2,4 @@ num = int(input("enter a number : "))
 if(num%3==0 and num%10==4):
     print("yes divisible by 3 and last digit is 4 ")
 else:
-    print("NO!!! not divisible by 3 and last digit is 4") 
+    print(" not divisible by 3 and last digit is 4") 
